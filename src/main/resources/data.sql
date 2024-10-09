15dd9fd8eec94b4727d70aff4da6abdcff8190b0
Amina Alibasic <alibasicamina@outlook.com>
2024-10-09 14:01:08 +0200

    Modified database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO category (name) VALUES ('Appetizer');
-INSERT INTO category (name) VALUES ('Main Course');
-INSERT INTO category (name) VALUES ('Dessert');
-INSERT INTO category (name) VALUES ('Breakfast');
+INSERT INTO category (name) VALUES ('Appetizer') ON CONFLICT (name) DO NOTHING;;
+INSERT INTO category (name) VALUES ('Main Course') ON CONFLICT (name) DO NOTHING;;
+INSERT INTO category (name) VALUES ('Dessert') ON CONFLICT (name) DO NOTHING;;
+INSERT INTO category (name) VALUES ('Breakfast') ON CONFLICT (name) DO NOTHING;;
